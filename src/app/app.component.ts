8b88ef056e770248b5e11d4cf6ada68d3ac77ca8
Jatinamin <jatinamin23@gmail.com>
2022-07-09 14:04:24 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, VERSION } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  
+  // fName: string;
+  // lName: string;
+  // city: string ;
+  // state: string;
+  
+  onSubmit(form:NgForm) {
+    
+    
+    console.log(form.value);
+  }
+ 
+}
+
+
+
